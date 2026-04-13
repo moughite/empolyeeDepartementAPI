34075e5b1d12cc1c34f867bcbe506101f1c09647
Moughite Hanafi <hanafi_moughite@yahoo.fr>
2026-04-13 17:09:40 +0200

    Initial commit

---
+++ src/test/java/com/hr/empolyeeapi/EmpolyeeapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hr.empolyeeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpolyeeapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
