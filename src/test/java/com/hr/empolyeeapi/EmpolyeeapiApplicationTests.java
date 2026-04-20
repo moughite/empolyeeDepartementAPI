615312ecba120b9deca25298ac01b854261b8ce4
Moughite Hanafi <hanafi_moughite@yahoo.fr>
2026-04-20 18:41:56 +0200

    feat(Ticket2): sonar issue fixen

--- src/test/java/com/hr/empolyeeapi/EmpolyeeapiApplicationTests.java
+++ src/test/java/com/hr/empolyeeapi/EmpolyeeapiApplicationTests.java
@@ -6,10 +6,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class EmpolyeeapiApplicationTests {
 
+	@Autowired
+	private ApplicationContext context;
+
 	@Test
 	void contextLoads() {
 		// Überprüft, dass der Spring Boot-Kontext korrekt startet
 		assertThat(context).isNotNull();
 	}
-
 }
